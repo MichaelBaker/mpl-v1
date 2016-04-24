9a87030259b56de8038145f1cff17c3e461f2aaa
Michael Baker <michaeltbaker@gmail.com>
2016-04-24 17:49:54 -0500

    Adds a second grammar rule

--- src/Mpl/Parser.hs
+++ src/Mpl/Parser.hs
@@ -2,12 +2,14 @@
 
 module Mpl.Parser where
 
+import Control.Applicative
 import Data.Text   (Text)
 import Text.Earley ((<?>), Grammar, Report, Prod, rule, fullParses, token)
 
 import qualified Text.Earley as E
 
-data AST = Int Text
+data AST = App Text AST
+         | Int Text
          deriving (Show, Eq)
 
 parse :: [Text] -> ([AST], Report Text [Text])
@@ -15,5 +17,6 @@ parse = fullParses (E.parser grammar)
 
 grammar :: Grammar r (Prod r Text Text AST)
 grammar = mdo
+  app <- rule $ App <$> token "f" <*> int <* token ")" <?> "application"
   int <- rule $ Int <$> token "1" <?> "integer"
-  return int
+  return (int <|> app)
--- test/Mpl/ParserSpec.hs
+++ test/Mpl/ParserSpec.hs
@@ -2,6 +2,7 @@ module Mpl.ParserSpec where
 
 import Test.Hspec
 
+import qualified Data.Text as T
 import Mpl.Parser (AST(..), parse)
 
 spec :: Spec
@@ -10,3 +11,8 @@ spec = do
     it "parses a single number" $ do
       let (parses, _) = parse ["1"]
       parses `shouldBe` [Int "1"]
+
+    it "parses prefix function application" $ do
+      let (parses, r) = parse $ T.words "f 1 )"
+      parses `shouldBe` [App "f" (Int "1")]
+
